c78bc783c0cf23ac9f08bfa1b1289b41bcf94b15
samw7823 <srwiener@gmail.com>
2015-11-07 18:00:18 -0500

    Added initial code for creating the symbol table in the compile.ml file

--- compile.ml
+++ compile.ml
@@ -3,3 +3,9 @@ open Bytecode
 
 module StringMap = Map.Make(String)
 
+(* Environemnt: symbol tables for functions, global and local vars *)
+type env = {
+	function_index 		: int StringMap.t; 	(* Index for each function *)
+	global_index 		: int StringMap.t; 	(* Address for global vars *)
+	local_index 		: int StringMap.t; 	(* FB offset for args, locals *)
+}
