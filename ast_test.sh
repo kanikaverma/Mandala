d1b3767609423333adac24fd9397c8e1f6b9fc70
samw7823 <srwiener@gmail.com>
2015-11-07 23:13:15 -0500

    Updated ast, parser and scanner files. Need to also update the parser to remove conflict from running ast_test.sh

---
+++ ast_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+PARSE='
+open Ast;;\n
+\n
+let lexbuf Lexing.from_channel stdin in\n
+Parser.program Scanner.token lexbuf;;'
+(echo -e $PARSE; cat -) | ocaml scanner.cmo parser.cmo
