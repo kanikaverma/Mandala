8260d2c606d368d8a3a62a7b6d43970c20311f76
Kanika Verma <vermakanika@hotmail.com>
2015-11-12 22:12:47 -0500

    added ID checking and find_variable

--- compiler/semantic.ml
+++ compiler/semantic.ml
@@ -1,14 +1,16 @@
 open Ast
 open Sast
 
+exception Error of string
+
 (*symbol table *)
 type symbol_table={
 	parent : symbol_table option;
-	variables: variable_decl list
+	variables: var_decl list
 }
 
 type function_table={
-	functions: function_decl list
+	functions: func_decl list
 } 
 
 (*envioronment*)
@@ -16,4 +18,26 @@ type translation_enviornment ={
 	var_scope: symbol_table;
 	fun_scope: function_table;
 }
+
+let rec find_variable (scope: symbol_table) name=
+	try
+		List.find (fun (_,s) -> s = name) scope.variables
+	with Not_found ->
+		match scope.parent with
+			Some(parent)-> find_variable parent name
+		| _ -> raise Not_found
+
+let rec expr env = function
+
+Ast.Id(vname) ->
+	let vdecl = try
+		find_variable env.var_scope vname
+	with Not_found ->
+		raise (Error("undeclared identifier"^vname))
+	in 
+	let (typ,_) =vdecl in 
+	typ, Sast.Id(vdecl)
+
+
+
 
