974d6f8d7465a90adda085f14c255c6ba07fb6d7
Kanika Verma <vermakanika@hotmail.com>
2015-11-12 20:16:22 -0500

    symbol table, function table and translation enviornment

--- compiler/semantic.ml
+++ compiler/semantic.ml
@@ -1,8 +1,19 @@
 open Ast
 open Sast
 
+(*symbol table *)
 type symbol_table={
 	parent : symbol_table option;
 	variables: variable_decl list
+}
+
+type function_table={
+	functions: function_decl list
+} 
+
+(*envioronment*)
+type translation_enviornment ={
+	var_scope: symbol_table;
+	fun_scope: function_table;
 }
 
