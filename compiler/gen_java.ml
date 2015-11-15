d86907e5df3d2bd6531160535bd997d31a4eaa17
edoroth <edoroth@gmail.com>
2015-11-15 07:24:11 +0000

    Small modification to java generation

--- compiler/gen_java.ml
+++ compiler/gen_java.ml
@@ -10,13 +10,16 @@ let sast =
 	Semantic.semantic_check ast
 
 let gen_java s =
+	if (List.length (fst s) > 0 || List.length (snd s) > 0) then (
 	print_string "public class Program {\n\n";
-  print_string "\tpublic static void main(String[] args) {\n\n";
+ 	print_string "\tpublic static void main(String[] args) {\n\n";
 	print_string "\t\tTurtle t = new Turtle();\n";
     print_string "\t\tt.hide();\n";
     print_string "\t\tt.setPosition(0, 0);\n";
     print_string "\t\tt.dot();\n";
-    print_string "\t}\n\n}"
+    print_string "\t}\n\n}"; )
+
+	else print_string "EDO FOUND AN ARBITRARY ERROR"
 
 let _ =	
 	gen_java sast
