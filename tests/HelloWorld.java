d5aedf7794a379c9faa4be5ded0e56e573e62efd
edoroth <edoroth@gmail.com>
2015-11-15 00:23:26 +0000

    Easier to put all in main

--- tests/HelloWorld.java
+++ tests/HelloWorld.java
@@ -1,13 +1,9 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    drawDot(0, 0);
-  }
-
-  public static void drawDot(int x, int y) {
     Turtle t = new Turtle();
     t.hide();
-    t.setPosition(x, y);
+    t.setPosition(0, 0);
     t.dot();
   }
 
