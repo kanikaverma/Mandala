457b582961ab8f43c009be87964cb1555d1c61fe
Harsha Vemuri <hv2169@columbia.edu>
2015-11-15 02:15:02 -0500

    hello world automation

--- compiler/semantic.sh
+++
@@ -1,9 +0,0 @@
-filename=$1
-sanitized_filename=${filename%.*.*}$".cpp"
-
-echo "#include <iostream>" > $sanitized_filename
-echo "using namespace std;" >> $sanitized_filename
-echo "" >> $sanitized_filename
-echo "int main(){" >> $sanitized_filename
-echo "  cout << 5 << endl;" >> $sanitized_filename
-echo "};" >> $sanitized_filename
