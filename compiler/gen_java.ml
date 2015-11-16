6937cecc6b7f0f8a69a66eaa594710788cdfa462
vagrant <vagrant@precise32.(none)>
2015-11-16 05:54:58 +0000

    gen_java.ml generates based on stmt list, in process of solving parser error with Mandala create

--- compiler/gen_java.ml
+++ compiler/gen_java.ml
@@ -4,22 +4,45 @@ open Sast
 open Semantic
 open Lexing
 
+exception Error of string
+
 let sast =
 	let lexbuf = Lexing.from_channel stdin in
 	let ast = Parser.program Scanner.token lexbuf in
 	Semantic.semantic_check ast
 
-let gen_java s =
-	if (1 > 0 ) then (
-	print_string "public class Program {\n\n";
- 	print_string "  public static void main(String[] args) {\n\n";
-	print_string "    Turtle t = new Turtle();\n";
-  print_string "    t.hide();\n";
-  print_string "    t.setPosition(0, 0);\n";
-  print_string "    t.dot();\n";
-  print_string "  }\n\n}"; )
+let proc_expr =function
+	Sast.Call(fname, actual_args)->
+		print_string "    t.dot();\n"
 
-	else print_string "EDO FOUND AN ARBITRARY ERROR"
+
+let proc_stmt = function
+	Sast.Mandala(vname) ->
+		(*print java code for mandala of this name*)
+		print_string "    Turtle t = new Turtle();\n";
+		print_string "    t.hide();\n";
+		print_string "    t.setPosition(0, 0);\n";
+	| Sast.Expr(expression)->
+		proc_expr expression
+	| _ -> raise (Error("unsupported statement found")) 
+
+let gen_java = function
+	Sast.SProg(s)-> 
+		let x = List.length s in
+		if (x>0) then (
+			print_string "public class Program {\n\n";
+ 			print_string "  public static void main(String[] args) {\n\n";
+		
+
+			List.map proc_stmt s; 
+
+			print_string "  }\n\n}";
+			let x_string = string_of_int x in
+			print_string x_string;
+
+		)	
+
+		else print_string "No input provided"
 
 let _ =	
 	gen_java sast
