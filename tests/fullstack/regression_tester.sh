922274db0cddd60fea32e55d113088e13c20ad00
Harsha Vemuri <hv2169@columbia.edu>
2015-12-18 16:19:03 -0500

    new regression testing capabilities

---
+++ tests/fullstack/regression_tester.sh
@@ -0,0 +1,59 @@
+# Automated regression testing on full programs
+# Functionality for multiple files included 
+
+#!/bin/bash
+
+preprocessor="../../compiler/preprocessor.py"
+semantic="../../compiler/semantic.sh"
+run="../../compiler/run"
+j_file="Program.java"
+exe="Program"
+expected="Expected.java"
+warnings="../tests/fullstack/warnings.txt"
+compare="compare.py"
+
+# BUILDING
+
+cd ../../compiler
+echo "" > $warnings 
+echo "" > $expected
+make 2> $warnings
+cd ../tests/fullstack
+
+# GET ALL MANDALA FILES 
+mandala_files=$(find suite -name *\.mandala)
+
+for m_file in $mandala_files
+do
+  # PREPROCESSING
+  python $preprocessor $m_file
+  p_file=$(find suite -name *\.proc) 
+
+  # JAVA GENERATION
+  ./$run < $p_file > "suite/Program.java" & 
+
+  # JAVA COMPILATION
+  cd suite 
+  javac $j_file
+
+  # EXECUTION 
+  java $exe
+
+  # COMPARING
+  diff=$(python $compare output.jpg Program.jpg)
+
+  if [[ $diff -eq 0 ]]; then
+    echo "Output Correct: [y]"
+  else
+    echo "Output Correct: [ ]"
+  fi
+
+  # CLEANING 
+  echo "done"
+  rm -f *.proc
+  mv Turtle.java Turtle.java.keep
+  rm -f *.java
+  mv Turtle.java.keep Turtle.java
+  rm -f *.class
+  cd ..
+done
